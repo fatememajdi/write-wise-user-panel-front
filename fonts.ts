9101c77ca941089588bcd78ccf1b2d2bd99919ce
Fateme <76264392+fatememajdi@users.noreply.github.com>
2023-07-16 00:25:18 +0330

    add fonts

---
+++ fonts.ts
@@ -0,0 +1,12 @@
+import { Inter, Roboto_Mono, Lato } from 'next/font/google'
+
+export const inter = Inter({
+    subsets: ['latin'],
+    display: 'swap',
+})
+
+export const roboto_mono = Lato({
+    subsets: ['latin'],
+    display: 'swap',
+    weight: '100'
+})
